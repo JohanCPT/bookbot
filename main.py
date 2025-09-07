833b3476553daf1bd21ed3071f28b3696c22665d
JohanP <johanpieterscpt@gmail.com>
2025-09-07 13:35:20 +0200

    Added Charachter count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,6 +9,8 @@ def main():
     book_content = get_book_text("books/frankenstein.txt")
     #print(book_content)
     number_of_words = word_count(book_content)
+    number_of_chars = char_count(book_content)
     print(f"{number_of_words} words found in the document")
-
+    print(number_of_chars)
+    
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,22 @@
 def word_count(content_string):
     words_list = content_string.split()
-    #print(words_list[13000:])
+    if __name__ == "main":
+        print(words_list[0:100])
     return len(words_list)
+
+def char_count(content_string):
+    
+    #First Convert the string to lowercase
+    lower_string = content_string.lower()
+    
+    #now loop through the lowercase string checking whether the char exists
+    #in the dictionary, and then incrementing the count if it does
+    char_count_dict = {}
+
+    for char in lower_string:
+        if char in char_count_dict:
+            char_count_dict[char] = char_count_dict[char] + 1
+        else:
+            char_count_dict[char] = 1
+
+    return char_count_dict
