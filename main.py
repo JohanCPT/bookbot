0487c79cbb33c3004fbb91f28ebfeae36e4397ce
JohanP <johanpieterscpt@gmail.com>
2025-09-15 22:03:00 +0200

    Made it possible to pass file path as a parameter to the program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, char_count, sort_by_count_decreasing
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,21 +7,26 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book_content = get_book_text(book_path)
-    number_of_words = word_count(book_content)
-    number_of_chars = char_count(book_content)
-    list_of_pairs = sort_by_count_decreasing(number_of_chars)
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        book_content = get_book_text(book_path)
+        number_of_words = word_count(book_content)
+        number_of_chars = char_count(book_content)
+        list_of_pairs = sort_by_count_decreasing(number_of_chars)
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}")
-    print("----------- Word Count ----------")
-    print(f" Found {number_of_words} total words")
-    print("--------- Character Count -------")
-     
-    for dict_pair in list_of_pairs:
-        if dict_pair["name"].isalpha():
-            print(f"{dict_pair["name"]}: {dict_pair["num"]}")
-    print("============= END ===============")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}")
+        print("----------- Word Count ----------")
+        print(f" Found {number_of_words} total words")
+        print("--------- Character Count -------")
+        
+        for dict_pair in list_of_pairs:
+            if dict_pair["name"].isalpha():
+                print(f"{dict_pair["name"]}: {dict_pair["num"]}")
+        print("============= END ===============")
 
 main()
